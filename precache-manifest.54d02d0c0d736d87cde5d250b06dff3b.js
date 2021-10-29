72a311a99c321323c0a5bc4fd5015ea3b46f97ff
Saied Gaffer <saied2421998@gmail.com>
2021-10-29 13:58:36 +0200

    Updates

---
+++ precache-manifest.54d02d0c0d736d87cde5d250b06dff3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544ae2cbfc99d17b2cce281cd75e5343",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "00bc6e10bc7af4b4b048",
+    "url": "/robofriends-react/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "d5672b5360d4e0701215",
+    "url": "/robofriends-react/static/css/main.090a5a52.chunk.css"
+  },
+  {
+    "revision": "00bc6e10bc7af4b4b048",
+    "url": "/robofriends-react/static/js/2.393514e0.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/robofriends-react/static/js/2.393514e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5672b5360d4e0701215",
+    "url": "/robofriends-react/static/js/main.f67f61b1.chunk.js"
+  },
+  {
+    "revision": "4a26307f729ee557879e",
+    "url": "/robofriends-react/static/js/runtime-main.b0d87439.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends-react/precache-manifest.60d9a8f3db59572993e3c0850013c3b5.js"
+  "/robofriends-react/precache-manifest.54d02d0c0d736d87cde5d250b06dff3b.js"
 );
 
 self.addEventListener('message', (event) => {
