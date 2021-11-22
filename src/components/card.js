3411921be77b7f38c6cec08315ea3fb7167a7dd2
unknown <saied2421998@gmail.com>
2021-11-22 14:24:52 +0200

    improve pwa

--- src/components/card.js
+++ src/components/card.js
@@ -3,8 +3,8 @@ import {memo} from 'react';
 const Card = ({id,name,email}) => {
       return (
       	        <div className="tc pa2 ma2 br3 bg-light-yellow grow shadow-5">
-                    <img alt="robot" src={`https://robohash.org/${id}?300x300`} 
-                      width="300" height="300" type="image/avif" />
+                    <img alt="robot" src={`https://robohash.org/${id}?200x200`} 
+                      width="200" height="200"/>
                  	<h2>{name}</h2>
                  	<p>{email}</p>
                  </div>
