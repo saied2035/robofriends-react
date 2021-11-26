b6ac306b58af1710cb247e2269f3ad5f19d818be
unknown <saied2421998@gmail.com>
2021-11-26 15:45:45 +0200

    try to add more security

--- src/service-worker.js
+++ src/service-worker.js
@@ -63,8 +63,8 @@ registerRoute(
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+self.addEventListener('message', event => {
+  if (event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
